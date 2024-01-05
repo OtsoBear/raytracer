41705b02635ba8e9b5deb808250e0e03236fddb5
Otso-Veistera <otso.veistera@eduespoo.fi>
2024-01-05 23:41:39 +0200

    Made ray.rs file for ray managing and integrated it with main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 mod vec;
-use std::io::{stderr, Write};
-use vec::{Vec3, Color};
+mod ray;
+
+use vec::{Vec3, Point3, Color};
+use ray::Ray;
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = r.direction().normalized();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
+    // Image
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u64 = 256;
-    const IMAGE_HEIGHT: u64 = 256;
+    const IMAGE_HEIGHT: u64 = ((256 as f64) / ASPECT_RATIO) as u64;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0
+                          - Vec3::new(0.0, 0.0, focal_length);
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -15,9 +36,12 @@ fn main() {
         stderr().flush().unwrap();
 
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new((i as f64) / ((IMAGE_WIDTH - 1) as f64),
-                                         (j as f64) / ((IMAGE_HEIGHT - 1) as f64),
-                                         0.25);
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
+
+            let r = Ray::new(origin,
+                             lower_left_corner + u * horizontal + v * vertical - origin);
+            let pixel_color = ray_color(&r);
 
             println!("{}", pixel_color.format_color());
         }
