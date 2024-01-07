0a2a1a0b6bbcf1f15f31d5f940d9cfa79deb5815
Otso-Veistera <otso.veistera@eduespoo.fi>
2024-01-07 20:39:36 +0200

    Made file more concise and reduced number of lines

--- src/vec.rs
+++ src/vec.rs
@@ -12,21 +12,11 @@ pub type Color = Vec3;
 
 impl Vec3 {
     pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
-        Vec3 {
-            e: [e0, e1, e2]
-        }
+        Vec3 { e: [e0, e1, e2] }
     }
-    pub fn x(self) -> f64 {
-        self[0]
-    }
-    
-    pub fn y(self) -> f64 {
-        self[1]
-    }
-    
-    pub fn z(self) -> f64 {
-        self[2]
-    }
+    pub fn x(self) -> f64 {self[0]}
+    pub fn y(self) -> f64 {self[1]}
+    pub fn z(self) -> f64 {self[2]}
     
     pub fn dot(self, other: Vec3) -> f64 {
         self[0] * other[0] + self[1] * other[1] + self[2] * other[2]
@@ -49,10 +39,9 @@ impl Vec3 {
     pub fn normalized(self) -> Vec3 {
         self / self.length()
     }
+    
     pub fn format_color(self) -> String {
-        format!("{} {} {}", (255.999 * self[0]) as u64,
-                            (255.999 * self[1]) as u64,
-                            (255.999 * self[2]) as u64)
+        format!("{} {} {}", (255.999 * self[0]) as u64, (255.999 * self[1]) as u64, (255.999 * self[2]) as u64)
     }
 }
 
@@ -74,17 +63,13 @@ impl Add for Vec3 {
     type Output = Vec3;
 
     fn add(self, other: Vec3) -> Vec3 {
-        Vec3 {
-            e: [self[0] + other[0], self[1] + other[1], self[2] + other[2]]
-        }
+        Vec3 { e: [self[0] + other[0], self[1] + other[1], self[2] + other[2]] }
     }
 }
 
 impl AddAssign for Vec3 {
     fn add_assign(&mut self, other: Vec3) -> () {
-        *self = Vec3 {
-            e: [self[0] + other[0], self[1] + other[1], self[2] + other[2]]
-        };
+        *self = Vec3 { e: [self[0] + other[0], self[1] + other[1], self[2] + other[2]] };
     }
 }
 
@@ -92,17 +77,13 @@ impl Sub for Vec3 {
     type Output = Vec3;
 
     fn sub(self, other: Vec3) -> Vec3 {
-        Vec3 {
-            e: [self[0] - other[0], self[1] - other[1], self[2] - other[2]]
-        }
+        Vec3 { e: [self[0] - other[0], self[1] - other[1], self[2] - other[2]] }
     }
 }
 
 impl SubAssign for Vec3 {
     fn sub_assign(&mut self, other: Vec3) -> () {
-        *self = Vec3 {
-            e: [self[0] - other[0], self[1] - other[1], self[2] - other[2]]
-        };
+        *self = Vec3 { e: [self[0] - other[0], self[1] - other[1], self[2] - other[2]] };
     }
 }
 
@@ -110,17 +91,13 @@ impl Mul<f64> for Vec3 {
     type Output = Vec3;
 
     fn mul(self, other: f64) -> Vec3 {
-        Vec3 {
-            e: [self[0] * other, self[1] * other, self[2] * other]
-        }
+        Vec3 { e: [self[0] * other, self[1] * other, self[2] * other] }
     }
 }
 
 impl MulAssign<f64> for Vec3 {
     fn mul_assign(&mut self, other: f64) -> () {
-        *self = Vec3 {
-            e: [self[0] * other, self[1] * other, self[2] * other]
-        };
+        *self = Vec3 { e: [self[0] * other, self[1] * other, self[2] * other] };
     }
 }
 
@@ -128,9 +105,7 @@ impl Mul<Vec3> for f64 {
     type Output = Vec3;
 
     fn mul(self, other: Vec3) -> Vec3 {
-        Vec3 {
-            e: [self * other[0], self * other[1], self * other[2]]
-        }
+        Vec3 { e: [self * other[0], self * other[1], self * other[2]] }
     }
 }
 
@@ -138,17 +113,13 @@ impl Div<f64> for Vec3 {
     type Output = Vec3;
 
     fn div(self, other: f64) -> Vec3 {
-        Vec3 {
-            e: [self[0] / other, self[1] / other, self[2] / other]
-        }
+        Vec3 { e: [self[0] / other, self[1] / other, self[2] / other] }
     }
 }
 
 impl DivAssign<f64> for Vec3 {
     fn div_assign(&mut self, other: f64) -> () {
-        *self = Vec3 {
-            e: [self[0] / other, self[1] / other, self[2] / other]
-        };
+        *self = Vec3 { e: [self[0] / other, self[1] / other, self[2] / other] };
     }
 }
 
