d444340c1a090929b79f97846a12a18d28fd2bf4
Otso-Veistera <otso.veistera@eduespoo.fi>
2024-01-08 17:30:39 +0200

    Added function to check if ray hits sphere, and integrated it with the ray color

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,22 @@ use vec::{Vec3, Point3, Color};
 use ray::Ray;
 use std::io::Write;
 
+
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
 // Calculates the color of a ray in a ray tracing context.
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     // The `unit_direction` is the normalized direction vector of the ray.
     let unit_direction = r.direction().normalized();
     // `t` is calculated as half of the sum of the y-component of the `unit_direction` vector and 1.0.
