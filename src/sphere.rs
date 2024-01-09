f3a241dac334bcb10bb0d217f80c646444348219
Otso-Veistera <otso.veistera@eduespoo.fi>
2024-01-09 20:07:18 +0200

    Created sphere.rs for the sphere object

---
+++ src/sphere.rs
@@ -0,0 +1,50 @@
+use super::vec::{Point3, Vec3};
+use super::ray::Ray;
+use super::hit::{Hit, HitRecord};
+
+pub struct Sphere {
+    center: Point3,
+    radius: f64
+}
+
+impl Sphere {
+    pub fn new(cen: Point3, r: f64) -> Sphere {
+        Sphere {
+            center: cen,
+            radius: r
+        }
+    }
+}
+
+impl Hit for Sphere {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<hitrecord> {
+        let oc = r.origin() - self.center;
+        let a = r.direction().length().powi(2);
+        let half_b = oc.dot(r.direction());
+        let c = oc.length().powi(2) - self.radius.powi(2);
+        
+        let discriminant = half_b.powi(2) - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        // Find the nearest root that lies in the acceptable range
+        let sqrtd = discriminant.sqrt();
+        let mut root = (-half_b - sqrtd) / a;
+        if root < t_min || t_max < root {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || t_max < root {
+                return None;
+            }
+        }
+
+        let p = r.at(root);
+        let rec = HitRecord {
+            t: root,
+            p: p,
+            normal: (p - self.center) / self.radius;
+        };
+
+        Some(rec)
+    }
+}
